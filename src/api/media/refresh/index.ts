373f4e7cf6bf1d43ea3345429e808b41dfba05a9
Corey Merritt <coreyleemerritt@gmail.com>
2024-10-21 19:46:42 -0400

    Clarified some responses

--- src/api/media/refresh/index.ts
+++ src/api/media/refresh/index.ts
@@ -11,34 +11,34 @@ router.post('/:mediaType?', async (req, res) => {
         switch(tableToRefresh) {
             case MediaFileDataTypes.Movies:
                 Database.refreshTable(DatabaseTables.MovieFileData, '/mnt/z/media/videos/movies', [ '.mkv', '.avi', '.mp4', '.mov' ])
-                res.sendStatus(200)
+                res.status(200).send(`Refreshing ${DatabaseTables.MovieFileData}\n`)
                 break
             case MediaFileDataTypes.Shows:
                 Database.refreshTable(DatabaseTables.ShowFileData, '/mnt/z/media/videos/shows', [ '.mkv', '.avi', '.mp4', '.mov' ])
-                res.sendStatus(200)
+                res.status(200).send(`Refreshing ${DatabaseTables.ShowFileData}\n`)
                 break
             case MediaFileDataTypes.Standup:
                 Database.refreshTable(DatabaseTables.StandupFileData, '/mnt/z/media/videos/standup', [ '.mkv', '.avi', '.mp4', '.mov' ])
-                res.sendStatus(200)
+                res.status(200).send(`Refreshing ${DatabaseTables.StandupFileData}\n`)
                 break
             case MediaFileDataTypes.Anime:
                 Database.refreshTable(DatabaseTables.AnimeFileData, '/mnt/z/media/videos/anime', [ '.mkv', '.avi', '.mp4', '.mov' ])
-                res.sendStatus(200)
+                res.status(200).send(`Refreshing ${DatabaseTables.AnimeFileData}\n`)
                 break
             case MediaFileDataTypes.Animation:
                 Database.refreshTable(DatabaseTables.AnimationFileData, '/mnt/z/media/videos/animation', [ '.mkv', '.avi', '.mp4', '.mov' ])
-                res.sendStatus(200)
+                res.status(200).send(`Refreshing ${DatabaseTables.AnimationFileData}\n`)
                 break
             case MediaFileDataTypes.Internet:
                 Database.refreshTable(DatabaseTables.InternetFileData, '/mnt/z/media/videos/internet', [ '.mkv', '.avi', '.mp4', '.mov' ])
-                res.sendStatus(200)
+                res.status(200).send(`Refreshing ${DatabaseTables.InternetFileData}\n`)
                 break
             default:
-                res.sendStatus(400)
+                res.status(400).send(`Bad params.\n`)
                 break
         }
     } else {
-        res.sendStatus(200)
+        res.status(200).send(`Refreshing all tables.\n`)
         await Database.refreshTable(DatabaseTables.MovieFileData, '/mnt/z/media/videos/movies', [ '.mkv', '.avi', '.mp4', '.mov' ])
         await Database.refreshTable(DatabaseTables.ShowFileData, '/mnt/z/media/videos/shows', [ '.mkv', '.avi', '.mp4', '.mov' ])
         await Database.refreshTable(DatabaseTables.StandupFileData, '/mnt/z/media/videos/standup', [ '.mkv', '.avi', '.mp4', '.mov' ])
